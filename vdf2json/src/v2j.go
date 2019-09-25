e06fc70ca729ec7acf6fc4150a2bc94825078d97
sujiahong <1511877962@qq.com>
2019-09-25 10:33:31 +0800

    优化

--- vdf2json/src/v2j.go
+++ vdf2json/src/v2j.go
@@ -2,15 +2,10 @@ package main
 
 import (
 	"bufio"
-	"bytes"
 	"fmt"
 	"io"
 	"os"
 )
-
-func describe(i interface{}) {
-	fmt.Printf("Type = %T, value = %v\n", i, i)
-}
 
 func readVdf(fileName string, m *map[string]interface{})  {
 	f, err := os.Open(fileName)
@@ -18,7 +13,6 @@ func readVdf(fileName string, m *map[string]interface{})  {
 		return
 	}
 	defer f.Close()
-	var appid string
 	var curM = *m
 	var mapArr []map[string]interface{}
 	br := bufio.NewReader(f)
@@ -27,59 +21,45 @@ func readVdf(fileName string, m *map[string]interface{})  {
 		if err == io.EOF {
 			break
 		}
-		if appid != "" {
-			var startIdx = -1
-			var kvArr []string
-			var bracesChar string
-			for i, char := range arrByte {
-				if string(char) == "\"" {
-					if startIdx < 0 {
-						startIdx = i + 1
-					} else {
-						kvArr = append(kvArr, string(arrByte[startIdx:i]))
-						startIdx = -1
-					}
-				} else if string(char) == "{" {
-					bracesChar = "{"
-					break
-				} else if string(char) == "}" {
-					bracesChar = "}"
-					break
+		var startIdx = -1
+		var kvArr []string
+		var bracesChar string
+		for i, char := range arrByte {
+			if string(char) == "\"" {
+				if startIdx < 0 {
+					startIdx = i + 1
+				} else {
+					kvArr = append(kvArr, string(arrByte[startIdx:i]))
+					startIdx = -1
 				}
-			}
-			switch bracesChar {
-			case "{":
+			} else if string(char) == "{" {
+				bracesChar = "{"
 				break
-			case "}":
-				curM = mapArr[len(mapArr)-1]
-				mapArr = mapArr[:len(mapArr)-1]
+			} else if string(char) == "}" {
+				bracesChar = "}"
 				break
-			default:
-				arrLen := len(kvArr)
-				if (arrLen > 1){
-					curM[kvArr[0]] = kvArr[1]
-				}else{
-					mapArr = append(mapArr, curM)
-					curM[kvArr[0]] = make(map[string]interface{})
-					curM = curM[kvArr[0]].(map[string]interface{})
-				}
-			}
-			kvArr = kvArr[0:0]
-			startIdx = -1
-			bracesChar = ""
-		} else {
-			arr := bytes.Split(arrByte, []byte(","))
-			fmt.Println(len(arr), cap(arr))
-			if len(arr) > 1 {
-				for _, item := range arr {
-					idx := bytes.Index(item, []byte("AppID"))
-					if idx > -1 {
-						appid = string(item[8:])
-						break
-					}
-				}
 			}
 		}
+		switch bracesChar {
+		case "{":
+			break
+		case "}":
+			curM = mapArr[len(mapArr)-1]
+			mapArr = mapArr[:len(mapArr)-1]
+			break
+		default:
+			arrLen := len(kvArr)
+			if (arrLen > 1){
+				curM[kvArr[0]] = kvArr[1]
+			}else if(arrLen == 1){
+				mapArr = append(mapArr, curM)
+				curM[kvArr[0]] = make(map[string]interface{})
+				curM = curM[kvArr[0]].(map[string]interface{})
+			}
+		}
+		kvArr = kvArr[0:0]
+		startIdx = -1
+		bracesChar = ""
 	}
 }
 
